ba214f03e443a6890f791dde4e3fa114c78b2c8f
fana-sowell <fana@sowell.app>
2024-03-20 11:04:24 +0300

    ui-733-creer-un-component-pour-afficher-les-infos-de-chaque-user

---
+++ src/components/User/ListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-item clickable v-ripple>
+    <q-item-section avatar>
+      <q-avatar color="primary" text-color="white">
+        {{ label.charAt(0) }}
+      </q-avatar>
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ label }}</q-item-label>
+      <q-item-label caption>{{ caption }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+// import { User } from 'src/models/UserInterface';
+
+defineProps<{
+  label: string;
+  caption: string;
+}>();
+
+</script>
