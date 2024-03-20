1302886b18ada9ec83f6ce7d19e9a6a40b2dca3f
nfanorenana <nyonjafanorenana@gmail.com>
2024-03-20 10:55:25 +0300

    ui-731-ticket-ny-onja
    - Modification des noms de variable
    - Utilisation de computed
    - Utilisation de v-for="(item, index)...

--- src/pages/ListPage.vue
+++ src/pages/ListPage.vue
@@ -1,39 +1,31 @@
 <template>
   <div class="q-pa-md">
-    <q-input
-      v-model="search"
-      filled
-      type="search"
-      hint="Search"
-      @update:model-value="searchName()"
-    >
+    <q-input v-model="search" filled type="search" hint="Search">
       <template v-slot:append>
         <q-icon name="search" />
       </template>
     </q-input>
     <q-list dense bordered padding class="rounded-borders">
-      <q-item v-ripple v-for="d in dataList" :key="d.index">
-        <q-item-section> {{ d.name }} </q-item-section>
+      <q-item v-ripple v-for="(item, index) in filteredUsersList" :key="index">
+        <q-item-section> {{ item.name }} </q-item-section>
       </q-item>
     </q-list>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import data from './data.json';
 
-var dataList = data;
+const usersList = ref(data);
 
 const search = ref('');
 
-const searchName = () => {
-  const val = search.value.toLowerCase();
+const filteredUsersList = computed(() => {
+  const searchValue = search.value.toLowerCase();
 
-  const temp = data.filter(function (d) {
-    return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+  return usersList.value.filter((user) => {
+    return user.name.toLowerCase().indexOf(searchValue) !== -1 || !searchValue;
   });
-
-  dataList = temp;
-};
+});
 </script>
