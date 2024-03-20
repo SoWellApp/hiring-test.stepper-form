20375cc6b4c1e17488c56ef856b411549965c2f8
nfanorenana <nyonjafanorenana@gmail.com>
2024-03-20 09:58:29 +0300

    ui-735-creer-un-component-pour-afficher-les-infos-de-chaque-user

---
+++ src/components/UserCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card class="my-card" flat bordered>
+      <img src="https://cdn.quasar.dev/img/parallax2.jpg" />
+
+      <q-list>
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="primary" name="local_bar" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Name</q-item-label>
+            <q-item-label caption
+              >{{ user.name.firstname }} {{ user.name.lastname }}</q-item-label
+            >
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="red" name="local_gas_station" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Address</q-item-label>
+            <q-item-label caption>{{ user.address.city }}</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable>
+          <q-item-section avatar>
+            <q-icon color="amber" name="local_movies" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label>Email</q-item-label>
+            <q-item-label caption>{{ user.email }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { User } from 'src/types/interfaces';
+
+defineProps<{ user: User }>();
+</script>
+
+<style lang="scss" scoped>
+.my-card {
+  width: 100%;
+  max-width: 300px;
+}
+</style>
