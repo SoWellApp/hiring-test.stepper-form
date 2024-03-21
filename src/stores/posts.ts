526ff7c6be0d858398a33c4b60f998238b5725ea
rafenitra-sowell <rafenitra@sowell.app>
2024-03-21 09:28:56 +0300

    ui-737-charger-plus-de-posts-recherche

--- src/stores/posts.ts
+++ src/stores/posts.ts
@@ -7,29 +7,64 @@ import { postCollection } from '../boot/pouchorm';
 export const usePostStore = defineStore('posts', () => {
   const isLoading = ref(false);
   const posts = ref<Post[]>([]);
+  const postsPerPage = 10;
+  let currentPage = 1;
 
-  const loadPosts = async () => {
+  const initPost = async () => {
     isLoading.value = true;
     posts.value = [];
-
     try {
+      const getFromPouchOrm = await postCollection.find({});
+      await postCollection.bulkRemove(getFromPouchOrm);
+
       const response = await api.get<Post[]>(
         'https://jsonplaceholder.typicode.com/posts'
       );
       if (response.status === 200) {
-        await postCollection.bulkUpsert(response.data);
-        const getFromPouchOrm = await postCollection.find({}, { limit: 10 });
-        posts.value = getFromPouchOrm;
+        const updatePostId = response.data.map((post) => ({
+          _id: post.id.toString(),
+          ...post,
+        }));
+        await postCollection.bulkUpsert(updatePostId);
+        posts.value = updatePostId;
       }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const loadPosts = async (page: number) => {
+    console.log('loadPosts work');
+    isLoading.value = true;
+    try {
+      const startIndex = (page - 1) * postsPerPage;
+
+      const allPosts = await postCollection.find();
+      const sortedPosts = allPosts.sort((a, b) => a.id - b.id);
+      const pagePosts = sortedPosts.slice(
+        startIndex,
+        startIndex + postsPerPage
+      );
+
+      posts.value = [...posts.value, ...pagePosts];
     } catch (error) {
       console.log('🚀 ~ loadPosts ~ error:', error);
       posts.value = [];
+    } finally {
+      isLoading.value = false;
     }
+  };
+
+  const loadMorePosts = async () => {
+    currentPage++; // Incrémenter le numéro de page
+    await loadPosts(currentPage);
   };
 
   return {
     isLoading,
     posts,
+    initPost,
     loadPosts,
+    loadMorePosts,
   };
 });
