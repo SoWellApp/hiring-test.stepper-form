cc0cf30420ca0e790c60ee2932f230db059b9057
fana-sowell <fana@sowell.app>
2024-03-26 13:54:18 +0300

    ui-750-sw-patrimony-search: add input search

---
+++ src/components/PatrimonyComponents/InputSearch.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-input
+    class="search-input"
+    color="transparent"
+    filled
+    v-model="searchInput">
+    <template v-slot:prepend>
+      <q-icon name="search" size="30px" />
+    </template>
+  </q-input>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const searchInput = ref<string>("")
+</script>
+
+<style scoped>
+.search-input {
+  min-width: 344px;
+  font-size: 16px;
+}
+</style>
---
+++ src/pages/ReporterPage/PatrimonyPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <InputSeach />
+  </main>
+</template>
+
+<script setup lang="ts">
+import InputSeach from "src/components/PatrimonyComponents/InputSearch.vue"
+</script>
+
+<style lang="css" scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  height: 100vh;
+  padding: 7rem;
+}
+</style>
